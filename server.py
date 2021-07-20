aaba184fc7582e037a616dabca9733910edd4207
y-futamura <49299955+y-futamura@users.noreply.github.com>
2021-07-21 08:39:11 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -1,22 +1,4 @@
-from flask import Flask
-import os
+import datetime
 
-PORT = 8080
-name = os.environ.get('NAME')
-if name == None or len(name) == 0:
-  name = "world"
-MESSAGE = "Hello, " + name + "!"
-print("Message: '" + MESSAGE + "'")
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def root():
-  print("Handling web request. Returning message.")
-  result = MESSAGE.encode("utf-8")
-  return result
-
-
-if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=PORT)
+dt_now = datetime.datetime.now()
+print('今日は', dt_now.year, '年', dt_now.month, '月', dt_now.day, '日')
